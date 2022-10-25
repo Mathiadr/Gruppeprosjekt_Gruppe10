a7d69e6f7e93890cbfe893bdb6e71abac428f0cc
Starynkiewicz <95142247+Starynkiewicz@users.noreply.github.com>
2022-10-25 12:00:46 +0200

    Made user class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+
 public class Car {
     private String registrationNumber;
     private String owner;
     private String model;
+    private String availableDate;
+
+    private static ArrayList<Car> carArrayList = new ArrayList<>();
 
     public Car(String registrationNumber, String owner, String model) {
         this.registrationNumber = registrationNumber;
@@ -9,6 +14,22 @@ public class Car {
         this.model = model;
     }
 
+
+    public String getAvailableDate() {
+        return availableDate;
+    }
+
+    public void setAvailableDate(String availableDate) {
+        this.availableDate = availableDate;
+    }
+
+    public static ArrayList<Car> getCarArrayList() {
+        return carArrayList;
+    }
+
+    public static void setCarArrayList(ArrayList<Car> carArrayList) {
+        Car.carArrayList = carArrayList;
+    }
 
     public String getRegistrationNumber() {
         return registrationNumber;
