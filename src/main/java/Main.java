59f3ebe9ea9877e7434176c699444b8c3ad54167
mdmth915 <martinvestby@hotmail.com>
2022-11-01 11:29:06 +0100

    GUI form & CarRental class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import forms.CarRental;
+
+
 public class Main {
+    public static void main(String[] args) {
+        CarRental carRental = new CarRental("Car rental");
+        carRental.setVisible(true);
+    }
 }
